659f55dd30810ed3d0b5bdef7e365b451bba68cb
Femi-Fernandez <43548033+Femi-Fernandez@users.noreply.github.com>
2019-10-03 12:34:56 +0100

    Initial commit

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Rigidbody rb;
+    public float horizontalSpeed = 4.0f;
+    public float forwardSpeed = 400.0f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(transform.right * forwardSpeed, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(-transform.right * forwardSpeed, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(transform.forward * forwardSpeed, ForceMode.Acceleration);
+        }
+  
+
+    }
+}
