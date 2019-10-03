4132a7cb73de296a9df6c83e327dc85c390974c1
Femi-Fernandez <43548033+Femi-Fernandez@users.noreply.github.com>
2019-10-03 12:45:53 +0100

    Got camera to follow player and added basic player movement

---
+++ Assets/cameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    public GameObject objectToFollow;
+    public float speed = 2.0f;
+   Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - objectToFollow.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredPosition = objectToFollow.transform.position + offset;
+        transform.position = desiredPosition;
+    }
+}
