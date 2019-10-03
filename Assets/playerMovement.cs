3529d8ae376a03be67b338b568af4105d989711d
Femi-Fernandez <43548033+Femi-Fernandez@users.noreply.github.com>
2019-10-03 13:01:06 +0100

    trying to refine movement

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -7,7 +7,7 @@ public class playerMovement : MonoBehaviour
     // Start is called before the first frame update
 
     Rigidbody rb;
-    public float horizontalSpeed = 4.0f;
+    public float horizontalSpeed = 1.0f;
     public float forwardSpeed = 400.0f;
     void Start()
     {
@@ -18,14 +18,15 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rb.AddForce(transform.right * forwardSpeed, ForceMode.Acceleration);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb.AddForce(-transform.right * forwardSpeed, ForceMode.Acceleration);
-        }
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    rb.AddForce(transform.right * forwardSpeed, ForceMode.Force);
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    rb.AddForce(-transform.right * forwardSpeed, ForceMode.Force);
+        //}
+
         if (Input.GetKey(KeyCode.UpArrow))
         {
             rb.AddForce(transform.forward * forwardSpeed, ForceMode.Acceleration);
