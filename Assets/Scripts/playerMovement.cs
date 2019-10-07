18f191c10584faa65766a53c6ca67bdf15c9b5e6
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2019-10-07 16:47:58 +0100

    created boost script and moved left right so it can be done when over max speed

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -8,40 +8,69 @@ public class playerMovement : MonoBehaviour
 
     Rigidbody rb;
     public float horizontalSpeed = 8.0f;
-    public float forwardSpeed = 20.0f;
-    public float maxSpeed = 100.0f;
+
+    float forwardSpeed;
+    public float boostForwardSpeed = 40.0f;
+    public float baseForwardSpeed = 20.0f;
+ 
+
+    float speed;
+    public float maxSpeed = 200.0f;
+    public static float baseMaxSpeed = 100.0f;
+    public float current;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
 
+        //set acceleration and max speed to default values
+        speed = baseMaxSpeed;
+        forwardSpeed = baseForwardSpeed;
     }
-
+     
     // Update is called once per frame
     void Update()
     {
+        current = rb.velocity.magnitude;
+        movePlayer();
+    }
 
-        if (rb.velocity.magnitude < maxSpeed)
+    void movePlayer()
+    {
+        //when boost button is pressed, set speed and acceleration values to their boost values
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.position += (transform.right * horizontalSpeed) * Time.deltaTime;
-            }
+            speed = maxSpeed;
+            forwardSpeed = boostForwardSpeed;
+        }
+        else
+        {
+            //when boost is released, reset to base values.
+            speed = baseMaxSpeed;
+            forwardSpeed = baseForwardSpeed;
+        }
 
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.position += (-transform.right * horizontalSpeed) * Time.deltaTime;
-
-            }
+        //if speed is less than max speed allow acceleration
+        if (rb.velocity.magnitude < speed)
+        {
 
             if (Input.GetKey(KeyCode.UpArrow))
             {
                 rb.AddForce(transform.forward * forwardSpeed, ForceMode.Acceleration);
             }
-        }  
-        
+        }
 
+        //left and right doesnt use forces as it needs to feel snappy
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += (transform.right * horizontalSpeed) * Time.deltaTime;
+        }
 
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += (-transform.right * horizontalSpeed) * Time.deltaTime;
 
+        }
     }
 }
