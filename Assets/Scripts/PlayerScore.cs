bb312c58ea408484a6735896b457977e69910028
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2019-11-02 17:39:19 +0000

    Player gets more score the closer to an obstacle they dodge

--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -26,8 +26,26 @@ public class PlayerScore : MonoBehaviour
     public void ScoreCalculate(float dist)
     {
         score = Mathf.RoundToInt(dist);
-        totalScore += score;
-
-        scoreText.text = "Current Score: " + totalScore;
+        
+        if (dist >= 40)
+        {
+            totalScore += score;
+            scoreText.text = "Current Score: " + totalScore;
+        }
+        else if ((dist < 40) && (dist >= 25))
+        {
+            totalScore += score * 2;
+            scoreText.text = "Current Score: " + totalScore;
+        }
+        else if ((dist < 25) && (dist >= 10))
+        {
+            totalScore += score * 3;
+            scoreText.text = "Current Score: " + totalScore;
+        }
+        else if ((dist < 10))
+        {
+            totalScore += score * 5;
+            scoreText.text = "Current Score: " + totalScore;
+        }
     }
 }
