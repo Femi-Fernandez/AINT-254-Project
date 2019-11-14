8a0dcf95dc9def6151960b3f41a4795ab6a373f9
Femi-Fernandez <43548033+Femi-Fernandez@users.noreply.github.com>
2019-11-14 15:24:31 +0000

    got a pause button to pause the game, and a resume button on the UI to resume the game

---
+++ Assets/Scripts/PlayerPause.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerPause : MonoBehaviour
+{
+    public GameObject inGameUI;
+    public GameObject pauseScreenUI;
+
+    private bool paused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inGameUI.SetActive(true);
+        pauseScreenUI.SetActive(false);
+        paused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {         
+                pauseScreen();     
+        }
+    }
+
+    public void pauseScreen()
+    {
+        paused = true;
+        inGameUI.SetActive(false);
+        pauseScreenUI.SetActive(true);
+        Time.timeScale = 0;        
+    }
+    public void unpauseScreen()
+    {
+        paused = false;
+        inGameUI.SetActive(true);
+        pauseScreenUI.SetActive(false);
+        Time.timeScale = 1;
+
+    }
+}
