0628b917fb38a1b6b9d84efd57849bed8fbb9962
Femi-Fernandez <43548033+Femi-Fernandez@users.noreply.github.com>
2019-11-14 14:58:34 +0000

    setup the start button, exit button, and level select buttons.

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public GameObject mainButtons;
+    public GameObject levelSelectButtons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainButtons.SetActive(true);
+        levelSelectButtons.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void levelSelect()
+    {
+        mainButtons.SetActive(false);
+        levelSelectButtons.SetActive(true);
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+}
