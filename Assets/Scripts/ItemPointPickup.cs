05a3ffd186e8dd0f2dcff413f8268d2190e99c63
Femi-Fernandez <43548033+Femi-Fernandez@users.noreply.github.com>
2019-11-14 13:41:33 +0000

    changed pointpickup script so that it automatically finds the player

--- Assets/Scripts/ItemPointPickup.cs
+++ Assets/Scripts/ItemPointPickup.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class ItemPointPickup : MonoBehaviour
 {
-    public GameObject player;
+    private GameObject player;
 
     public int pointValue = 50;
     // Start is called before the first frame update
     void Start()
     {
         //playerScore = gameObject.GetComponent<PlayerScore>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void OnTriggerEnter(Collider col)
