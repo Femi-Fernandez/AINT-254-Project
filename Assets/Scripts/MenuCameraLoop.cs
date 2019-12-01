14b5d5f77721eac09bc0999e3461f7ba7c3884d7
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2019-12-01 18:11:29 +0000

    created menu background art and made it autoscroll

---
+++ Assets/Scripts/MenuCameraLoop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraLoop : MonoBehaviour
+{
+    public float scrollSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,0,scrollSpeed);
+        if (transform.position.x <= -8)
+        {
+            transform.position = new Vector3(24, 0.67f, 0);
+        }
+    }
+}
